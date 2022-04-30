468d4d2241f3cb05ea0ae84a1467ebdfd03b4518
Navneeth Krishna <navneethkrishnaspai@gmail.com>
2022-05-01 01:55:03 +0530

    day 1

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
+import 'package:instagram_clone/responsive/responsive_screen_layout.dart';
+import 'package:instagram_clone/responsive/web_screen_layout.dart';
+void main()=> runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(), 
+        webScreenLayout: WebScreenLayout()
+        ),
+    );
+  }
+}
